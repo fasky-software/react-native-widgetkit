497fae4af1a3a832ec96f791598612637ffe8760
Thomas Leiter <thomas.leiter@outlook.at>
2020-10-24 16:44:20 +0200

    fix: reloadTimeLines crashes

--- ios/Widgetkit.m
+++ ios/Widgetkit.m
@@ -3,6 +3,6 @@
 @interface RCT_EXTERN_MODULE(Widgetkit, NSObject)
 
 RCT_EXTERN_METHOD(reloadAllTimelines)
-RCT_EXTERN_METHOD(reloadTimelines:(char[])ofKind)
+RCT_EXTERN_METHOD(reloadTimelines:(NSString *)ofKind)
 
 @end
